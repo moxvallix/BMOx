8b90eea130499c421dce8038f166de7bcd3dfaa3
Moxvallix <moxvallix@gmail.com>
2023-09-14 12:30:21 +0930

    feat(bot): add bot name for namespacing commands, and display help command in status

--- lib/bmox/bot.rb
+++ lib/bmox/bot.rb
@@ -11,48 +11,11 @@ class BMOx::Bot
       client_id: BMOx::CONFIG.dig(:bot, :client_id),
       prefix: BMOx::CONFIG.dig(:bot, :prefix) || "/"
     )
+    @name = (BMOx::CONFIG.dig(:bot, :name) || "bmox").to_s.downcase.gsub(/[^a-z0-9]/, "_")
     @typing = {}
     @characters = {}
     @queue = []
     @queue_loop = false
-    
-    @bot.command :bmox_list do |_event, *args|
-      str = "**META:** Available Characters\n"
-      @characters.each do |id, character|
-        str << "/#{id} - Talk to #{character.name}\n"
-      end
-      str
-    end
-
-    @bot.command :bmox_queue do |_event, *args|
-      "**META:** There are #{@queue.size + (@queue_loop ? 1 : 0)} prompts being processed currently."
-    end
-
-    @bot.command :bmox_help do |_event, *args|
-      "**META:** BMOx, by Moxvallix\nRun `/bmox_list` to see character commands!\nRun `/bmox_queue` to see the queue size."
-    end
-    
-    @bot.command :bmox_reload do |_event, *args|
-      if @queue.size > 0
-        "**META:** Queue is not empty!"
-      else
-        self.class.restart
-      end
-    end
-    
-    BMOx::PROMPT_DIR.glob("*.character").each do |template|
-      puts "Registering: #{template.basename}"
-      character = BMOx::Character.new(template.read)
-      unless character.prompt_id
-        puts "Character is missing a prompt id!"
-        next
-      end
-      @characters[character.prompt_id] = character
-      @bot.command character.prompt_id.to_s.to_sym do |event, *args|
-        queue_command(character: character, event: event, args: args)
-        nil
-      end
-    end
   end
 
   def process_response(event, args, output, character)
@@ -90,7 +53,59 @@ class BMOx::Bot
     end
   end
 
+  def on_ready
+    @bot.update_status("online", "#{@bot.prefix}#{@name}_help", nil, 0, false, 2)
+  end
+
   def start
+    register_base_commands
+    register_character_commands
+    @bot.ready { on_ready }
     @bot.run
+  end
+
+  private
+
+  def register_base_commands
+    @bot.command :"#{@name}_list" do |_event, *args|
+      str = "**META:** Available Characters\n"
+      @characters.each do |id, character|
+        str << "#{@bot.prefix}#{id} - Talk to #{character.name}\n"
+      end
+      str
+    end
+
+    @bot.command :"#{@name}_queue" do |_event, *args|
+      "**META:** There are #{@queue.size + (@queue_loop ? 1 : 0)} prompts being processed currently."
+    end
+
+    @bot.command :"#{@name}_help" do |_event, *args|
+      prefix = "#{@bot.prefix}#{@name}"
+      "**META:** BMOx, by Moxvallix\nRun `#{prefix}_list` to see character commands!\nRun `#{prefix}_queue` to see the queue size."
+    end
+    
+    @bot.command :"#{@name}_reload" do |_event, *_args|
+      @characters.each { |id, _| @bot.remove_command(:"#{id}") }
+      @characters = {}
+      register_character_commands
+      prefix = "#{@bot.prefix}#{@name}"
+      "**META:** Registered #{@characters.size} characters.\nRun `#{prefix}_list` to see character commands!"
+    end
+  end
+
+  def register_character_commands
+    BMOx::PROMPT_DIR.glob("*.character").each do |template|
+      puts "Registering: #{template.basename}"
+      character = BMOx::Character.new(template.read)
+      unless character.prompt_id
+        puts "Character is missing a prompt id!"
+        next
+      end
+      @characters[character.prompt_id] = character
+      @bot.command :"#{character.prompt_id}", aliases: [:"#{@name}:#{character.prompt_id}"] do |event, *args|
+        queue_command(character: character, event: event, args: args)
+        nil
+      end
+    end
   end
 end
