8b363c0bc3802b6245feb348c39748b74843bbee
Moxvallix <moxvallix@gmail.com>
2023-09-21 09:58:03 +0930

    feat(prompts): improve substitution, add chat character for more chatgpt style requests

--- lib/bmox/character.rb
+++ lib/bmox/character.rb
@@ -2,7 +2,8 @@ class BMOx::Character
   CONFIG = {
     "avatar" => "https://cdn.discordapp.com/avatars/1107979697700229142/08fc3e87798ef809b29e0ca3458bf0ae.webp",
     "name" => "Assistant",
-    "prompt_id" => nil
+    "prompt_id" => nil,
+    "reverse_prompt" => "### <username>:"
   }
 
   CONFIG.each do |key, _|
@@ -21,27 +22,37 @@ class BMOx::Character
   end
 
   def discord_author
-    Discordrb::Webhooks::EmbedAuthor.new(name: name, icon_url: avatar)
+    Discordrb::Webhooks::EmbedAuthor.new(name: @name, icon_url: @avatar)
   end
 
-  def formatted_prompt(additional_substitutes = {})
-    prompt = @prompt.dup
-    @initial_substitutes.merge(additional_substitutes).each do |key, value|
-      prompt.gsub! "<#{key}>", value.to_s
-    end
-    prompt
+  def substitute_string(string, additional_substitutes = {})
+    format_string(string, @initial_substitutes.merge(additional_substitutes))
   end
 
   def reply_to(message, substitutes = {}, params = {}, bindings = {})
-    substitutes.merge!(evaluate_variables(bindings))
-    prompt = formatted_prompt(substitutes.merge({prompt: message}))
-    params.merge!({"p": prompt})
+    subs = combine_hashes(@initial_substitutes, substitutes, evaluate_variables(bindings))
+    prompt = format_string(@prompt, subs.merge({prompt: message}))
+    r_prompt = format_string(@reverse_prompt, subs)
+    params.merge!({"p": prompt, "r": r_prompt})
+    puts @config
+    puts params
     # params.merge!({"-prompt-cache": prompt_cache_file, "-prompt-cache-all": nil, "-file": prompt_history_file})
     output = BMOx::Llama.generate(params)
     output.sub(prompt, "").strip
   end
 
   private
+
+  def combine_hashes(target = {}, *targets)
+    return {} unless target.is_a?(Hash)
+    targets.each { |hash| next unless hash.is_a?(Hash); target = target.merge(hash) }
+    target
+  end
+
+  def format_string(string, variables)
+    variables.each { |key, value| string = string.to_s.gsub("<#{key}>", value.to_s) }
+    string
+  end
 
   def variables_bind(bindings = {})
     OpenStruct.new({config: @config}.merge(bindings))
@@ -63,17 +74,17 @@ class BMOx::Character
   end
 
   def check_prompt_id
-    raise "Character missing a prompt id" unless prompt_id.is_a? String
-    raise "Prompt id contains invalid characters" if prompt_id.match? /[^a-z0-9_\-]/
+    raise "Character missing a prompt id" unless @prompt_id.is_a? String
+    raise "Prompt id contains invalid characters" if @prompt_id.match? /[^a-z0-9_\-]/
   end
 
   def prompt_cache_file
     check_prompt_id
-    BMOx::MEMORY_DIR.join("#{prompt_id}.prompt-cache")
+    BMOx::MEMORY_DIR.join("#{@prompt_id}.prompt-cache")
   end
 
   def prompt_history_file
     check_prompt_id
-    BMOx::MEMORY_DIR.join("#{prompt_id}.prompt-history")
+    BMOx::MEMORY_DIR.join("#{@prompt_id}.prompt-history")
   end
 end
